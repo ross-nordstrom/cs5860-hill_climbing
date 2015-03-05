2eb05ff8bd7470cd48104e52ce428285a12f68f6
Ross Nordstrom <nordstrom.ross@gmail.com>
2015-03-05 00:39:06 -0700

    update basic

--- n-queens/public/js/controllers/BasicCtrl.js
+++ n-queens/public/js/controllers/BasicCtrl.js
@@ -1,14 +1,16 @@
 angular.module('BasicCtrl', [])
     .controller('BasicController', ['$scope', 'Queens', function ($scope, Queens) {
 
-        $scope.attemptsLimit = 100;
+        $scope.attemptsLimit = 1;
+        $scope.maxAttempts = 0;
+        $scope.attemptsDisabled = true;
         $scope.numQueens = 8;
         $scope.queensBoard = null; //Queens.randomBoard($scope.numQueens);
 
-        $scope.regenerate = function (num) {
+        $scope.regenerate = function (maxAttempts, num) {
             $scope.queensBoard = Queens.randomBoard(num);
         };
 
         // Initialize
-        $scope.regenerate($scope.numQueens);
+        $scope.regenerate($scope.maxAttempts, $scope.numQueens);
     }]);
