a9043e72bff666f59ed8679d7dbb387857414ab1
Ross Nordstrom <nordstrom.ross@gmail.com>
2015-03-05 08:34:11 -0700

    working on showing debug info

--- n-queens/public/js/controllers/BasicCtrl.js
+++ n-queens/public/js/controllers/BasicCtrl.js
@@ -12,10 +12,12 @@ angular.module('BasicCtrl', [])
             boardObj.board = Queens.transpose(boardObj.board);
             $scope.queensBoard = boardObj;
 
+            $scope.queensBoard.iterations = 0;
             $scope.iteration($scope.queensBoard);
         };
 
         $scope.iteration = function (boardObj) {
+            // Untranspose for operating on
             boardObj.board = Queens.transpose(boardObj.board);
 
             var nextBoardObj = _.reduce(_.range(boardObj.queens.length), function (workingBoardObj, colIdx) {
@@ -23,6 +25,21 @@ angular.module('BasicCtrl', [])
             }, boardObj);
 
             $scope.queensBoard = Queens.mergeBoards($scope.queensBoard, nextBoardObj);
+            // Transpose for viewing
+            $scope.queensBoard.board = Queens.transpose($scope.queensBoard.board);
+            $scope.queensBoard.iterations++;
+        };
+
+        $scope.analyzeMoves = function (boardObj) {
+            // Untranspose for operating on
+            boardObj.board = Queens.transpose(boardObj.board);
+
+            var nextBoardObj = _.reduce(_.range(boardObj.queens.length), function (workingBoardObj, colIdx) {
+                return HillClimb.analyzeColumn(workingBoardObj, colIdx);
+            }, boardObj);
+
+            $scope.queensBoard = Queens.mergeBoards($scope.queensBoard, nextBoardObj);
+            // Transpose for viewing
             $scope.queensBoard.board = Queens.transpose($scope.queensBoard.board);
         };
 
@@ -35,8 +52,12 @@ angular.module('BasicCtrl', [])
             $scope.queensBoard = Queens.mergeBoards($scope.queensBoard, nextBoardObj);
             $scope.queensBoard.board = Queens.transpose($scope.queensBoard.board);
         };
+
+
+        // Common functions called by shared board.html
         $scope.columnClick = $scope.step;
         $scope.boardStep = $scope.iteration;
+        $scope.boardDebug = $scope.analyzeMoves;
 
         // Initialize
         $scope.regenerate($scope.maxAttempts, $scope.numQueens);
